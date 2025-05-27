53beb043c188a7ad5c7a0bbb1a97b29a891496d7
suehtamxx <eldermatheus60@gmail.com>
2025-05-27 20:41:50 -0300

    Revert "."
    This reverts commit c459c06f4e5089b00051e65889f4ea902f8a0b44.

--- comeco/mediaexec07.py
+++
@@ -1,5 +0,0 @@
-n = int(input('digite a 1° nota: '))
-n1 = int(input('digite a 2° nota: '))
-media = (n + n1)/2
-
-print('A media das notas é: {} ' .format(media))
--- comeco/soma.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Digite um numero: '))
-n1 += int(input('Digite outro valor: '))
-
-print('a soma é', n1)
